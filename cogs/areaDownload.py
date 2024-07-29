d0fa2ba4e0c607fb5e4070b3e82e8756f36d4788
daeltam <pachailare2@gmail.com>
2024-07-29 17:24:47 +0200

    Adding first custom emojis

--- cogs/areaDownload.py
+++ cogs/areaDownload.py
@@ -160,7 +160,7 @@ async def get_area(canvas_id, canvas, x, y, w, h, interaction : discord.Interact
                 rank += 1
                 percentage = (rank/total)*100
                 percentageBar = "".join(['🟩' for k in range(round(percentage/5))]+['🟫' for k in range(20-round(percentage/5))])
-                await interaction.edit_original_response(content = f"\:loading: Image processing {percentageBar}")  # : {percentage}% done.")
+                await interaction.edit_original_response(content = f"<a:loading:1267469203103940673> Image processing {percentageBar}")  # : {percentage}% done.")
         await asyncio.gather(*tasks)
         return target_matrix
 
@@ -285,10 +285,10 @@ class areaDownload(commands.Cog):
             matrix = await get_area(canvas_id, canvas_infos, x, y, w, h, interaction)
 
             image = await matrix.create_image() # send PIL image
-            await interaction.edit_original_response(content = "👌 Your image is ready, thank you for waiting ! ⏬ ")
+            await interaction.edit_original_response(content = "<a:shiny:1267483837148037190> Your image is ready, thank you for waiting ! ⏬ ")
             await interaction.channel.send(file = discord.File(fp=image, filename = "result.png"))
         except :
-            await interaction.edit_original_response("😖 Something went wrong, your image will not be delivered, please report a bug in the dedicated thread.")
+            await interaction.edit_original_response("<a:error40:1267490066125819907> Something went wrong, your image will not be delivered, please report a bug in the dedicated thread.")
         return
 
 async def setup(bot):
