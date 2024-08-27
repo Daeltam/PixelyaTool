bcde732ed2693763f012dbd331ebaba85b724e0c
daeltam <pachailare2@gmail.com>
2024-08-27 12:39:43 +0200

    bug fix

--- cogs/historyDownload.py
+++ cogs/historyDownload.py
@@ -295,7 +295,7 @@ class historyDownload(commands.Cog):
                     end_date = datetime.date.today()
                 else:
                     end_date = datetime.date.fromisoformat(end_date)
-                if end_date > start_date:
+                if end_date < start_date:
                     return await interaction.edit_original_response(content = "your end date is prior to your start date, this won't work.")
             except :
                 error_message= "<a:error40:1267490066125819907> Your date format is wrong and created an error, please make sure to use the YYYY-MM-DD format."
