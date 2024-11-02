134a4e597f7d4049c5b78fa83461b21265b82fde
daeltam <pachailare2@gmail.com>
2024-11-02 10:26:38 +0100

    bug fix on both functions

--- cogs/rankingCommands.py
+++ cogs/rankingCommands.py
@@ -42,8 +42,8 @@ class RankingCommands(commands.Cog):
                         title="Top 15 Daily Rankings by Pixels",
                         color=discord.Color.blue()
                     )
+                    rank = 1
                     for entry in top_15:
-                        rank = 1
                         name = entry['name']
                         if len(name) > 20:
                             name = name[:17] + "..."  
@@ -77,52 +77,49 @@ class RankingCommands(commands.Cog):
 
         async with aiohttp.ClientSession() as session:
             async with session.get("https://pixelya.fun/ranking") as response:
-                try :
-                    if response.status == 200:
-                        data = await response.json()
+                if response.status == 200:
+                    data = await response.json()
 
-                        ranking_data = data.get("ranking", [])
+                    ranking_data = data.get("ranking", [])
 
-                        if not ranking_data:
-                            await interaction.followup.send("No total rankings found.", ephemeral=True)
-                            return
+                    if not ranking_data:
+                        await interaction.followup.send("No total rankings found.", ephemeral=True)
+                        return
 
-                        top_15 = [ entry for entry in ranking_data[:15] if int(entry['r']) <16]
+                    top_15 = [ entry for entry in ranking_data[:15] if int(entry['r']) <16]
 
-                        embed = discord.Embed(
-                            title="Top 10 Total Rankings by Pixels",
-                            color=discord.Color.green()
-                        )
-                        rank = 1
-                        for entry in top_15:
-                            name = entry['name']
-                            if len(name) > 20:
-                                name = name[:17] + "..."  
+                    embed = discord.Embed(
+                        title="Top 10 Total Rankings by Pixels",
+                        color=discord.Color.green()
+                    )
+                    rank = 1
+                    for entry in top_15:
+                        name = entry['name']
+                        if len(name) > 20:
+                            name = name[:17] + "..."  
 
-                            if entry['facInfo'] is None :
-                                fac_tag =" "
-                            else :
-                                fac_tag = entry['facInfo'][0]
-                            total_pixels = str(entry['t']).replace(',', '')
-                            daily_total = str(entry['dt']).replace(',', '')
-                            urlname = urllib.parse.quote(name)
-                            print(entry)
-                            while rank < int(entry['r']):
-                                print("in while loop" ,  rank)
-                                embed.add_field(name=f"**{len(embed.fields)}**", value = "This user had a hidden profile so data aren't available.", inline = True)
-                                rank+= 1
+                        if entry['facInfo'] is None :
+                            fac_tag =" "
+                        else :
+                            fac_tag = entry['facInfo'][0]
+                        total_pixels = str(entry['t']).replace(',', '')
+                        daily_total = str(entry['dt']).replace(',', '')
+                        urlname = urllib.parse.quote(name)
+                        print(entry)
+                        while rank < int(entry['r']):
+                            print("in while loop" ,  rank)
+                            embed.add_field(name=f"**{len(embed.fields)}**", value = "This user had a hidden profile so data aren't available.", inline = True)
+                            rank+= 1
 
-                            embed.add_field(
-                                    name=f"**{entry['r']}**",
-                                    value=("""**Name:** [%s %s](https://pixelya.fun/profile?name=%s) \n **ID:** %s\n **Total:** %s\n **Daily:** %s\n"""%(fac_tag, name, urlname, entry['id'], total_pixels, daily_total)),
-                                    inline=True)
-                            rank += 1
+                        embed.add_field(
+                                name=f"**{entry['r']}**",
+                                value=("""**Name:** [%s %s](https://pixelya.fun/profile?name=%s) \n **ID:** %s\n **Total:** %s\n **Daily:** %s\n"""%(fac_tag, name, urlname, entry['id'], total_pixels, daily_total)),
+                                inline=True)
+                        rank += 1
 
-                        await interaction.followup.send(embed=embed)
-                    else:
-                        await interaction.followup.send("Failed to fetch rankings.", ephemeral=True)
-                except Exception:
-                    print(traceback.print_exc())
+                    await interaction.followup.send(embed=embed)
+                else:
+                    await interaction.followup.send("Failed to fetch rankings.", ephemeral=True)
 
     @rankings.command(name="best_daily", description="Display the ranking of who placed the most in one day")
     async def best_daily(self, interaction : discord.Interaction):
